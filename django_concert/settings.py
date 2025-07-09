d69bc4815edf7f6a658ed108cd7e8c932cb439b4
Nzoji Hipolito <71729152+nzorro-github@users.noreply.github.com>
2025-07-09 09:07:23 +0100

    Update settings.py
    Removed hard-coded secret

--- django_concert/settings.py
+++ django_concert/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-__a-u30pii=rp1qde9t@zdmykl=my#x0679qk#k895i7f#=+c="
+SECRET_KEY = f"{os.environ.get('DJANGO_SECRET')}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
